aa47255bb55a9de3bdde37136979c1ed8a1d56ce
Tomas Jimenez <40131693+tomasjimenez@users.noreply.github.com>
2020-06-17 21:38:13 -0500

    Update NuevoUsuario.js

--- src/components/NuevoUsuario.js
+++ src/components/NuevoUsuario.js
@@ -4,19 +4,18 @@ const NuevoUsuario = () => {
   return (
     <div>
       <div className="container">
-       
         <form className="needs-validation" novalidate>
   <div className="form-row">
     <div className="col-md-6 mb-3">
-      <label for="validationCustom01">First name</label>
-      <input type="text" className="form-control" id="validationCustom01" value="Mark" required />
+      <label for="validationCustom01">Nombre</label>
+      <input type="text" className="form-control" id="validationCustom01" value="Luis" required />
       <div className="valid-feedback">
         Looks good!
       </div>
     </div>
     <div className="col-md-6 mb-3">
-      <label for="validationCustom02">Last name</label>
-      <input type="text" className="form-control" id="validationCustom02" value="Otto" required />
+      <label for="validationCustom02">Apellidos</label>
+      <input type="text" className="form-control" id="validationCustom02" value="Perez" required />
       <div className="valid-feedback">
         Looks good!
       </div>
@@ -24,24 +23,26 @@ const NuevoUsuario = () => {
   </div>
   <div className="form-row">
     <div className="col-md-6 mb-3">
-      <label for="validationCustom03">City</label>
+      <label for="validationCustom03">Ciudad</label>
       <input type="text" className="form-control" id="validationCustom03" required />
       <div className="invalid-feedback">
         Please provide a valid city.
       </div>
     </div>
     <div className="col-md-3 mb-3">
-      <label for="validationCustom04">State</label>
+      <label for="validationCustom04">País</label>
       <select className="custom-select" id="validationCustom04" required>
-        <option selected disabled value="">Choose...</option>
-        <option>...</option>
+        <option selected disabled value="">Elige tu País</option>
+        <option>Ecuador</option>
+        <option>México</option>
+        <option>Colombia</option>
       </select>
       <div className="invalid-feedback">
         Please select a valid state.
       </div>
     </div>
     <div className="col-md-3 mb-3">
-      <label for="validationCustom05">Zip</label>
+      <label for="validationCustom05">Télefono</label>
       <input type="text" className="form-control" id="validationCustom05" required />
       <div className="invalid-feedback">
         Please provide a valid zip.
@@ -50,10 +51,6 @@ const NuevoUsuario = () => {
   </div>
   <div className="form-group">
     <div className="form-check">
-      <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-      <label className="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
       <div className="invalid-feedback">
         You must agree before submitting.
       </div>
