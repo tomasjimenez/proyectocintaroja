123547cb3ab3a672fafe24a9fa8b2a4b0b94bc77
Angelica Flores <martha.fuentes@broxel.com>
2020-06-17 21:33:26 -0500

    Estructura router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
 
-import login from './components/login';
-import Card from './components/Card';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import login from './components/login';
+import Profile from './components/Profile';
+import { BrowserRouter, Route } from 'react-router-dom';
+import CategoryContainer from './containers/CategoryContainer';
 
 
 
 function App() {
   return (
     <div>
-      <Header />
-      <CardContainer />
-      < Footer />
+      <BrowserRouter>
+        <Header />
+
+        <main>
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/login" component={login} />
+        </main>
+
+        < Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className="header">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">LOGO</a>
+                <Link className="navbar-brand" to="#">LOGO</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Mi Perfil</a>
+                            <Link className="nav-link" to="/profile">Mi Perfil</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Categorías
-                            </a>
+                            </Link>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
+                                <Link className="dropdown-item" to="#">Action</Link>
+                                <Link className="dropdown-item" to="#">Another action</Link>
                                 <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Something else here</a>
+                                <Link className="dropdown-item" to="#">Something else here</Link>
                             </div>
                         </li>
                     </ul>
                     <form className="form-inline my-4 my-lg-">
                         <input className="form-control mr-sm-2" type="search" placeholder="¿Qué quieres aprender hoy?" aria-label="Search" />
-                        <button className="btn text-light my-2 my-sm-0 bg-info" type="submit">Iniciar Sesión</button>
+                        <Link to="/login"><button className="btn text-light my-2 my-sm-0 bg-info" type="submit">Iniciar Sesión</button></Link>
                     </form>
                 </div>
             </nav>
