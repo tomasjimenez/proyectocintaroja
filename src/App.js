ed6d9dd168e3c8a235b811e692d21fa4224142d2
Gerardo Rodriguez <gerdomx@gmail.com>
2020-06-19 20:07:32 -0500

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Login from './components/Login';
+import Login from './components/login';
 import Profile from './components/Profile';
 import CourseView from './components/CourseView';
 import CategoryContainer from './containers/CategoryContainer';
@@ -20,7 +20,6 @@ function App() {
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/categories" component={CategoryContainer} />
-          <Route exact path="/login" component={login} />
           <Route exact path="/course" component={CourseView} />
         </main>
         < Footer />
